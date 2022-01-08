58f5b19f2d049b70c15c380eb05cc071e1cd98b9
Thakshila Nuwangi <thakshilanuwangi123@gmail.com>
2022-01-08 19:49:37 +0530

    Create UI

---
+++ src/controller/SimpleTextEditorController.java
@@ -0,0 +1,106 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class SimpleTextEditorController {
+    public Button btnNew;
+    public Button btnSave;
+    public Button btnReplace;
+    public Button btnReplace1;
+    public Button btnCut;
+    public Button btnCopy;
+    public Button btnOpen;
+    public Button btnSearch;
+    public TextField txtSearch;
+    public TextField txtReplace;
+    public Button btnFind;
+    public TextArea txtArea;
+    public Label lblWordCount;
+    public Label lblFoundCount;
+    public Button btnPaste;
+    public Button btnDown;
+    public Button btnUp;
+
+    public void mnuNewOnAction(ActionEvent actionEvent) {
+    }
+
+    public void mnuOpenOnAction(ActionEvent actionEvent) {
+    }
+
+    public void mnuSaveOnAction(ActionEvent actionEvent) {
+    }
+
+    public void mnuPrintOnAction(ActionEvent actionEvent) {
+    }
+
+    public void mnuExitOnAction(ActionEvent actionEvent) {
+    }
+
+    public void mnuCutOnAction(ActionEvent actionEvent) {
+    }
+
+    public void mnuCopyOnAction(ActionEvent actionEvent) {
+    }
+
+    public void mnuPasteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void mnuSelectAllOnAction(ActionEvent actionEvent) {
+    }
+
+    public void mnuFindOnAction(ActionEvent actionEvent) {
+    }
+
+    public void mnuReplaceOnAction(ActionEvent actionEvent) {
+    }
+
+    public void mnuReplaceAllOnAction(ActionEvent actionEvent) {
+    }
+
+    public void mnuAboutUsOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnNewOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnReplaceOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnCutOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnCopyOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnOpenOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSearchOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnUpOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDownOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnFindOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnPasteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void findUpOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void ownOnAction(MouseEvent mouseEvent) {
+    }
+}
